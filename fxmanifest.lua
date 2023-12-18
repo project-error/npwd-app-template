aedf1ff42f243c2673531b036aa678a4e914fdfc
itschip <chip@projecterror.dev>
2023-12-18 18:44:23 +0100

    update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version "cerulean"
 game "gta5"
 
-client_script 'dist/client.js'
-server_script 'dist/server.js'
+client_script "dist/client.js"
+server_script "dist/server.js"
 
-ui_page 'web/dist/index.html'
+ui_page "web/dist/index.html"
 
 files {
-    'web/dist/index.html',
-    'web/dist/*.js',
+	"web/dist/index.html",
+	"web/dist/**/*",
 }
