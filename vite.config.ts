6d7d45d6eddf5147c0afe19f5d7f30c603d90933
itschip <chip@projecterror.dev>
2023-12-18 18:34:40 +0100

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import topLevelAwait from 'vite-plugin-top-level-await'
+import topLevelAwait from 'vite-plugin-top-level-await';
 import federation from '@originjs/vite-plugin-federation';
 const packageJson = require('./package.json');
 const { dependencies, name } = packageJson;
 
-delete dependencies["@emotion/styled"];
-delete dependencies["@mui/material"];
-delete dependencies["@mui/styles"];
+delete dependencies['@emotion/styled'];
+delete dependencies['@mui/material'];
+delete dependencies['@mui/styles'];
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,27 +19,29 @@ export default defineConfig({
       exposes: {
         './config': './npwd.config.ts',
       },
-      shared: ["react", 'react-dom', "@emotion/react", "react-router-dom"]
+      shared: ['react', 'react-dom', '@emotion/react', 'react-router-dom'],
     }),
     topLevelAwait({
       // The export name of top-level await promise for each chunk module
-      promiseExportName: "__tla",
+      promiseExportName: '__tla',
       // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: i => `__tla_${i}`
-    })
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
   ],
   define: {
     process: {
-      env: { 
+      env: {
         VITE_REACT_APP_IN_GAME: process.env.VITE_REACT_APP_IN_GAME,
-      }
-    }
+      },
+    },
   },
   server: {
     port: 3002,
   },
   build: {
+    outDir: 'web/dist',
+    emptyOutDir: true,
     modulePreload: false,
-    outDir: 'web/dist'
-  }
+    assetsDir: '',
+  },
 });
